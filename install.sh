94dc6aba83730f4eba86103be0be6d728602fad1
binhex <megalith01@gmail.com>
2015-05-18 13:37:33 +0100

    add user nobody to wheel group and grant in sudoers

--- install.sh
+++ install.sh
@@ -7,6 +7,12 @@ pacman_packages="jenkins git unzip unrar sudo"
 pacman -Sy --noconfirm
 pacman -S --needed $pacman_packages --noconfirm
 
+# add wheel group to sudoers with no password
+echo "%wheel ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
+
+# add user nobody to wheel group
+usermod -G10 nobody
+
 # set permissions
 chown -R nobody:users /usr/share/java/jenkins/ /etc/conf.d/jenkins /var/cache/jenkins/
 chmod -R 775 /usr/share/java/jenkins/ /etc/conf.d/jenkins /var/cache/jenkins/
