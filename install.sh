14031029f31557952978b7e2e4f74eb05312440f
binhex <megalith01@gmail.com>
2015-11-12 17:27:01 +0000

    ref new base image, do full pacman upgrade

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ set -e
 pacman_packages="jenkins docker openssh"
 
 # install pre-reqs
-pacman -Sy --noconfirm
+pacman -Syu --ignore filesystem --noconfirm
 pacman -S --needed $pacman_packages --noconfirm
 
 # set permissions
