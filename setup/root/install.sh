d638025cf1f96f2cfcb62e047663b1a39d64d979
binhex <megalith01@gmail.com>
2016-11-03 20:20:20 +0000

    consolidate scripts

--- setup/root/install.sh
+++ setup/root/install.sh
@@ -3,20 +3,73 @@
 # exit script if return code != 0
 set -e
 
+# build scripts
+####
+
+# download build scripts from github
+curl -o /tmp/scripts-master.zip -L https://github.com/binhex/scripts/archive/master.zip
+
+# unzip build scripts
+unzip /tmp/scripts-master.zip -d /tmp
+
+# move shell scripts to /root
+find /tmp/scripts-master/ -type f -name '*.sh' -exec mv -i {} /root/  \;
+
+# pacman packages
+####
+
 # define pacman packages
-pacman_packages="git unzip unrar openssh"
+pacman_packages="git"
 
-# install pre-reqs
-pacman -S --needed $pacman_packages --noconfirm
+# install compiled packages using pacman
+if [[ ! -z "${pacman_packages}" ]]; then
+	pacman -S --needed $pacman_packages --noconfirm
+fi
 
-# set password for root user
-echo 'root:Jenkins' | chpasswd
- 
-# fix sshd for docker usage
-sed -i -e 's/^UsePAM yes/UsePAM no/g' /etc/ssh/sshd_config
+# aor packages
+####
+
+# define arch official repo (aor) packages
+aor_packages="jenkins"
+
+# define arch official repo (aor) package type e.g. core/community/extra
+aor_package_type="community"
 
 # call aor script (arch official repo)
 source /root/aor.sh
+
+# aur packages
+####
+
+# define aur helper
+aur_helper="apacman"
+
+# define aur packages
+aur_packages=""
+
+# call aur install script (arch user repo)
+source /root/aur.sh
+
+# container perms
+####
+
+# create file with contets of here doc
+cat <<'EOF' > /tmp/permissions_heredoc
+# set permissions inside container
+chown -R "${PUID}":"${PGID}" /usr/share/java/jenkins/ /etc/conf.d/jenkins /home/nobody /var/cache/
+chmod -R 775 /usr/share/java/jenkins/ /etc/conf.d/jenkins /home/nobody /var/cache/
+
+EOF
+
+# replace permissions placeholder string with contents of file (here doc)
+sed -i '/# PERMISSIONS_PLACEHOLDER/{
+    s/# PERMISSIONS_PLACEHOLDER//g
+    r /tmp/permissions_heredoc
+}' /root/init.sh
+rm /tmp/permissions_heredoc
+
+# env vars
+####
 
 # cleanup
 yes|pacman -Scc
