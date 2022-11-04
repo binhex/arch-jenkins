cea764d202b5039deefd5ffb8fcd06fa641cd182
binhex <megalith01@gmail.com>
2022-11-04 09:47:40 +0000

    remove ajpport

--- run/nobody/start.sh
+++ run/nobody/start.sh
@@ -8,8 +8,7 @@ export JENKINS_HOME=/config
 export JENKINS_WAR=/usr/share/java/jenkins/jenkins.war
 export JENKINS_WEBROOT=--webroot=/var/cache/jenkins
 export JENKINS_PORT=--httpPort=8090
-export JENKINS_AJPPORT=--ajp13Port=-1
-export JENKINS_COMMAND_LINE="$JAVA $JAVA_ARGS $JAVA_OPTS -jar $JENKINS_WAR $JENKINS_WEBROOT $JENKINS_PORT $JENKINS_AJPPORT $JENKINS_OPTS"
+export JENKINS_COMMAND_LINE="${JAVA} ${JAVA_ARGS} ${JAVA_OPTS} -jar ${JENKINS_WAR} ${JENKINS_WEBROOT} ${JENKINS_PORT} ${JENKINS_OPTS}"
 
 # run jenkins
-/bin/sh -c 'eval $JENKINS_COMMAND_LINE'
+/bin/sh -c 'eval "${JENKINS_COMMAND_LINE}"'
