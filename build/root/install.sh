22d900718bb1226a7d249e5f2a53c154a2e4e9a6
binhex <megalith01@gmail.com>
2024-07-30 11:42:31 +0100

    revert path

--- build/root/install.sh
+++ build/root/install.sh
@@ -62,7 +62,7 @@ source aur.sh
 ####
 
 # define comma separated list of paths
-install_paths="/usr/share/java/jenkins,/home/nobody,/var/cache,/var/cache/jenkins"
+install_paths="/usr/share/java/jenkins,/home/nobody,/var/cache"
 
 # split comma separated string into list for install paths
 IFS=',' read -ra install_paths_list <<< "${install_paths}"
